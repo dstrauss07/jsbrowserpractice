e920e7b3307748233e0949eb43b595982e616231
dstrauss <fmdenver@gmail.com>
2018-07-06 00:03:04 -0400

    tip 1

---
+++ js/tip.js
@@ -0,0 +1,8 @@
+var billTotal = document.getElementById("bill-total"),
+    fifteenPer = document.getElementById("fifteen-percent"),
+    twentyPer = document.getElementById("twenty-percent"),
+    twentyfivePer = document.getElementById("twentyfive-percent"),
+    tipTotal = document.getElementById("tip-total"),
+    billTotal= document.getElementById("bill-total");
+
+    
